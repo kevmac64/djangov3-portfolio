85f869b5afd5656924084123928c7b1144b98eba
kevmac64 <k_mac87@live.com>
2020-07-08 10:24:40 -0500

    start portfolio_app

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='portfolio/images')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.title}"
