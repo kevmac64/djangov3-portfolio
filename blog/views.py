34e29fc24a001fcf880e1afc3b933259f23540d2
kevmac64 <k_mac87@live.com>
2020-07-09 10:02:24 -0500

    Update for publish

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
 
 # Create your views here.
+def all_blogs(request):
+    #blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-date')  #for sorting
+    #Blog.objects.order_by('-date')[:5] for the last 5
+
+    return render(request, 'blog/all_blogs.html', {
+        'blogs': blogs,
+    })
+
+def detail(request, blog_id):
+    the_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {
+        'blog': the_blog,
+    })
